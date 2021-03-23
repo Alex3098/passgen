15fdd8323ccce01abbb8c72e38e1932cfd2f0a6f
a.yurchenko <a.yurchenko@geniusee.com>
2021-03-23 14:26:56 +0200

    Add logo, fix mobile version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import style from "./Navbar.module.css";
+import logo from '../img/passgen-logo.svg';
 
 export default function Navbar() {
   return (
     <div className={style.navbar}>
       <div className={style.container}>
-        <div className={style.logo}>PassGen</div>
+        <div className={style.logo}>
+          <img src={logo} alt="Passgen" />
+        </div>
       </div>
     </div>
   );
